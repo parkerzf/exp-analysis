da7425e298e3664df71b195dacfb6a72443aae97
Feng Zhao <feng.zhao@feedzai.com>
2016-05-01 00:17:20 +0100

    Add comments for group by model src code

--- src/model/predict_group_by_model.py
+++ src/model/predict_group_by_model.py
@@ -16,20 +16,39 @@ test = pd.read_csv(utils.raw_data_path + 'test.csv',
                     'orig_destination_distance', 'user_id'])
 
 def predict_group_by_model(group_by_field):
+	"""
+	Use group by model to predict the top 5 hotel clusters for the test data
+	:param group_by_field: group by field to get the related group by model 
+	:return: the dataframe with the submission format according to the model
+	"""
 	group_by_model = joblib.load(utils.model_path + 
 		'_'.join(['top', str(utils.k), 'cw', str(utils.click_weight), 'group', group_by_field, 'year', utils.train_year]) + '.pkl')
 	merged_df = test.merge(group_by_model, how='left',left_on= group_by_field, right_index=True)
 
 	merged_df.reset_index(inplace = True)
-	return merged_df[['index', 'hotel_cluster']]
+	result = merged_df[['index', 'hotel_cluster']]
+	result.columns =  ['id', 'hotel_cluster']
+	return result
 
-def fill_na(pre_result, group_by_field):
+def fill_na(prev_result, group_by_field):
+	"""
+	Use another group by model to enrich the previous model result for empty result instances
+	:param prev_result: the dataframe of the previous model result
+	:param group_by_field: group by field to get the another group by model
+	:return: the dataframe with the updated model result
+	"""
 	group_by_model = joblib.load(utils.model_path + \
 		'_'.join(['top', str(utils.k), 'cw', str(utils.click_weight), 'group', group_by_field, 'year', utils.train_year]) + '.pkl')
 	merged_df = test.merge(group_by_model, how='left',left_on= group_by_field, right_index=True)
-	pre_result.hotel_cluster.fillna(merged_df.hotel_cluster, inplace=True)
+	new_result = prev_result.hotel_cluster.fillna(merged_df.hotel_cluster, inplace=True)
 
 def fill_top_5(row, merged_df):
+	"""
+	The helper function for fill_all_top_5 function to merging a single result instance with another group by model result
+	:param row: the result instance to be filled
+	:param merged_df: another group by model result to merge to the row if the row has less than 5 hotel clusters
+	:return: the updated hotel clusters for this instance
+	"""
 	if not isinstance(row[1], float):
 			length = len(row[1].split())
 			if length < 5:
@@ -44,28 +63,34 @@ def fill_top_5(row, merged_df):
 		return merged_df.hotel_cluster[row[0]]
 
 
-def fill_all_top_5(pre_result, group_by_field):
+def fill_all_top_5(prev_result, group_by_field):
+	"""
+	Use another group by model to enrich the previous model result for result instances with less than 5 hotel clusters
+	:param prev_result: the dataframe of the previous model result
+	:param group_by_field: group by field to get the another group by model
+	:return: the dataframe with the updated model result
+	"""
 	group_by_model = joblib.load(utils.model_path + \
 		'_'.join(['top', str(utils.k), 'cw', str(utils.click_weight), 'group', group_by_field, 'year', utils.train_year]) + '.pkl')
 	merged_df = test.merge(group_by_model, how='left',left_on= group_by_field, right_index=True)
 
-	pre_result['new_hotel_cluster'] = pre_result.apply(lambda row: fill_top_5(row, merged_df), axis=1)
-	pre_result = pre_result.drop('hotel_cluster', 1)
-	pre_result.columns = ['id', 'hotel_cluster']
+	prev_result['new_hotel_cluster'] = prev_result.apply(lambda row: fill_top_5(row, merged_df), axis=1)
+	new_result = prev_result.drop('hotel_cluster', 1)
+	new_result.columns = ['id', 'hotel_cluster']
 
-	return pre_result
+	return new_result
 
 
 print 'predict with orig_destination_distance model...'
-merged_df = predict_group_by_model('orig_destination_distance')
+result = predict_group_by_model('orig_destination_distance')
 print 'predict with srch_destination_id model...'
-merged_df = fill_all_top_5(merged_df, 'srch_destination_id')
+result = fill_all_top_5(result, 'srch_destination_id')
 print 'predict with user_id model...'
-merged_df = fill_all_top_5(merged_df, 'user_id')
+result = fill_all_top_5(result, 'user_id')
 print 'predict with hotel_market model...'
-merged_df = fill_all_top_5(merged_df, 'hotel_market')
+result = fill_all_top_5(result, 'hotel_market')
 
-merged_df.hotel_cluster.to_csv(utils.model_path + 
+result.hotel_cluster.to_csv(utils.model_path + 
 	'results/submission_' + '_'.join(['top', str(utils.k), 'cw', str(utils.click_weight), 'year', utils.train_year]) + '.csv', 
 	header=True, index_label='id')
 
--- src/model/train_group_by_model.py
+++ src/model/train_group_by_model.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.externals import joblib
-
 import sys
 import os
+from sklearn.externals import joblib
+
 scriptpath = os.path.dirname(os.path.realpath(sys.argv[0])) + '/../'
 sys.path.append(os.path.abspath(scriptpath))
 import utils
@@ -21,11 +21,24 @@ train_2013 = train[train.date_time < '2014-01-01 00:00:00']
 train_2014 = train[train.date_time >= '2014-01-01 00:00:00']
 
 def top_k_relevence(group, k = utils.k):
+	"""
+	Order and get the topk hotel cluters by the relevance score in desc order
+	:param group: the aggregate group with hotel cluster relevance scores
+	:param k: the top k value
+	:return: the topk hotel clusters for the aggregate group
+	"""
     idx = group.relevance.nlargest(k).index
     top_k_relevence = group.hotel_cluster[idx].values
-    return np.array_str(top_k_relevence)[1:-1] # remove square brackets
+    return np.array_str(top_k_relevence)[1:-1]
 
 def gen_top_k_group_by_model(group_by_field, click_weight = utils.click_weight, year = 'all'):
+	"""
+	Generate and dump the group by model with top k hotel clusters 
+	:param group_by_field: group by field to generate the group with hotel cluster relevance scores
+	:param click_weight: the weight for the clicks
+	:param year: Year filter on training data
+	:return: the topk group by model with respect to group_by_field
+	"""
 	source = train
 	if year == '2013':
 		source = train_2013
@@ -34,8 +47,8 @@ def gen_top_k_group_by_model(group_by_field, click_weight = utils.click_weight, 
 	
 	agg = source.groupby([group_by_field, 'hotel_cluster'])['is_booking'].agg(['sum','count'])
 	agg['count'] -= agg['sum']
-	agg = agg.rename(columns={'sum':'bookings','count':'clicks'})
-	agg['relevance'] = agg['bookings'] + click_weight * agg['clicks']
+	agg = agg.rename(columns = {'sum':'bookings','count':'clicks'})
+	agg['relevance'] = agg['bookings'] + click_weight * agg['clicks'] # the weighted sum of bookings count and clicks count
 	agg.reset_index(inplace = True)
 	top_clusters = agg.groupby([group_by_field]).apply(top_k_relevence)
 	top_clusters = pd.DataFrame(top_clusters).rename(columns={0:'hotel_cluster'})
